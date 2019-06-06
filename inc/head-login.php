f271b3810c1c55003863122af2f3ef14ef0aeef2
Igor <dev@ifaidy.infini.fr>
2019-06-06 10:57:16 +0200

    meta robots

--- inc/head-login.php
+++ inc/head-login.php
@@ -1,5 +1,7 @@
 <head>
     <meta charset="<?php echo CHARSET ?>">
+    <meta name="robots" content="noindex, no follow">
+    <meta name="googlebot" content="noindex">
     <title><?php echo APP_NAME.' - '.COMPANY_NAME ?></title>
     <link rel="icon" type="image/png" href="<?php echo APP_FAVICON ?>">
     <link type="text/css" rel="stylesheet" href="<?php echo FOLDER_CSS ?>fa/all.min.css">
